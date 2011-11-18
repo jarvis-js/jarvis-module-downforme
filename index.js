4a92f2bd2249111371eabdaf86cdafff31972d5d
Alex Whitman <alex@alexwhitman.com>
2011-11-18 16:24:15 +0000

    Remove bot.Module.load() - no longer required
    - options are now assigned to module rather than passed in

--- index.js
+++ index.js
@@ -2,32 +2,30 @@ var http = require('http');
 
 module.exports = function(bot, module) {
 
-	module.load = function() {
-		module.addCommand(/^is (.*?) (?:up|down)(?:\?)?$/i, function(request, domain) {
-			var options = {
-				host: 'www.isup.me',
-				path: '/' + domain
+	module.addCommand(/^is (.*?) (?:up|down)(?:\?)?$/i, function(request, domain) {
+		var options = {
+			host: 'www.isup.me',
+			path: '/' + domain
+		}
+		http.get(options, function(res) {
+			if (res.statusCode == 200) {
+				var body = '';
+				res.on('data', function(chunk) {
+					body += chunk;
+				}).on('end', function() {
+					if (body.match('It\'s just you.')) {
+						request.reply = domain + ' looks UP from here';
+					}
+					else if (body.match('Huh?')) {
+						request.reply = domain + ' doesn\'t look like a domain';
+					}
+					else {
+						request.reply = domain + ' looks DOWN from here';
+					}
+					bot.respond(request);
+				});
 			}
-			http.get(options, function(res) {
-				if (res.statusCode == 200) {
-					var body = '';
-					res.on('data', function(chunk) {
-						body += chunk;
-					}).on('end', function() {
-						if (body.match('It\'s just you.')) {
-							request.reply = domain + ' looks UP from here';
-						}
-						else if (body.match('Huh?')) {
-							request.reply = domain + ' doesn\'t look like a domain';
-						}
-						else {
-							request.reply = domain + ' looks DOWN from here';
-						}
-						bot.respond(request);
-					});
-				}
-			});
 		});
-	};
+	});
 
 };
