6a69ff43d215813e96133fd5d6749e43dfa348ff
Alex Whitman <alex@alexwhitman.com>
2012-05-12 15:07:01 +0100

    Use an object to register commands and triggers

--- index.js
+++ index.js
@@ -2,30 +2,33 @@ var http = require('http');
 
 module.exports = function(bot, module) {
 
-	module.addCommand(/^is (.*?) (?:up|down)(?:\?)?$/i, function(request, domain) {
-		var options = {
-			host: 'www.isup.me',
-			path: '/' + domain
-		};
-		http.get(options, function(res) {
-			if (res.statusCode == 200) {
-				var body = '';
-				res.on('data', function(chunk) {
-					body += chunk;
-				}).on('end', function() {
-					if (body.match('It\'s just you.')) {
-						request.reply = domain + ' looks UP from here';
-					}
-					else if (body.match('Huh?')) {
-						request.reply = domain + ' doesn\'t look like a domain';
-					}
-					else {
-						request.reply = domain + ' looks DOWN from here';
-					}
-					bot.reply(request);
-				});
-			}
-		});
+	module.addCommand({
+		match: /^is (.*?) (?:up|down)(?:\?)?$/i,
+		func: function(request, domain) {
+			var options = {
+				host: 'www.isup.me',
+				path: '/' + domain
+			};
+			http.get(options, function(res) {
+				if (res.statusCode == 200) {
+					var body = '';
+					res.on('data', function(chunk) {
+						body += chunk;
+					}).on('end', function() {
+						if (body.match('It\'s just you.')) {
+							request.reply = domain + ' looks UP from here';
+						}
+						else if (body.match('Huh?')) {
+							request.reply = domain + ' doesn\'t look like a domain';
+						}
+						else {
+							request.reply = domain + ' looks DOWN from here';
+						}
+						bot.reply(request);
+					});
+				}
+			});
+		}
 	});
 
 };
